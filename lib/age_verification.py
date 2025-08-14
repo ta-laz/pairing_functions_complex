2d0a58d8917f00457f75fc04264de298b606dd9b
Alex Lazar <teodoraalexandra.lazar@gmail.com>
2025-08-14 15:16:01 +0100

    all tests passed

--- lib/age_verification.py
+++ lib/age_verification.py
@@ -3,14 +3,17 @@ from datetime import datetime, date
 def age_verification(DOB):
     today = datetime.today()
     sixteen_years_ago = date(today.year -16, today.month, today.day)
+    
     if DOB == "":
         raise Exception ("Please enter your date of birth")
     elif DOB != "":
         try: 
             DOB = datetime.strptime(DOB, "%Y-%m-%d").date()
-            print(datetime.today().year - 16)
             if DOB <= sixteen_years_ago:
                 return "Access granted"
+            else: 
+                age = today.year - DOB.year
+                return f"Access denied you are {age}, required age is 16"
 
         except ValueError: 
             raise Exception("Please enter your date of birth in YYYY-MM-DD format")  
