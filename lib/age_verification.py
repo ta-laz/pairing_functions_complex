3cadc4b7bc5391ee250749f5cbe00ff3f01fe5c2
Alex Lazar <teodoraalexandra.lazar@gmail.com>
2025-08-14 14:43:07 +0100

    test 3

--- lib/age_verification.py
+++ lib/age_verification.py
@@ -1,3 +1,11 @@
+from datetime import datetime 
+
 def age_verification(DOB):
     if DOB == "":
         raise Exception ("Please enter your date of birth")
+    elif DOB != "":
+        try: 
+            datetime.strptime(DOB, "%Y-%m-%d")
+            return True # we need to expand this
+        except ValueError: 
+            raise Exception("Please enter your date of birth in YYYY-MM-DD format")  
--- tests/test_age_verification.py
+++ tests/test_age_verification.py
@@ -11,3 +11,6 @@ def test_raise_exception_if_input_incorrect_format():
     with pytest.raises(Exception) as e:
         age_verification("12-12-12")
     assert str(e.value) == "Please enter your date of birth in YYYY-MM-DD format"
+
+def test_if_age_is_correct_and_over_16(): 
+    assert age_verification("2009-01-01") == "Access granted"
